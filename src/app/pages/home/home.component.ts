0a68ce40e9f1a8cfc28481829067a12a62d99334
mehakpreetkaurs <52216040+mehakpreetkaurs@users.noreply.github.com>
2023-08-15 11:37:14 -0400

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  data: any = null;
+
+  constructor(private apiService: ApiService) { }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.data = await this.apiService.searchGoogle();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
 }
