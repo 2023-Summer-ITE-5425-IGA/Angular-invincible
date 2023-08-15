f515195b42dab77da9ebc0e55890d2ca88e1c458
mehakpreetkaurs <52216040+mehakpreetkaurs@users.noreply.github.com>
2023-08-15 11:33:27 -0400

    Update events.component.ts

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
-import axios from 'src/app/axios';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios'; // Importing axios directly
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent {
-  events:any;
-  ngOnInit(): void {
-    this.fetchData()
-  }
-  async fetchData() {
-    try {
-      const response = await axios.get(`https://www.eventbriteapi.com/v3/users/me/?token=UW54HWESMNE2F7K2X2XU`);
-      const data = response.data;
-      console.log("Fetched data event:", data);
+export class EventsComponent implements OnInit {
+  events: any;
 
-      this.events = data
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    // Provided API options
+    const options = {
+      method: 'GET',
+      url: 'https://chicmi.p.rapidapi.com/calendar_in_city/',
+      params: {
+        city: 'london',
+        days: '5',
+        max_results: '3'
+      },
+      headers: {
+        'X-RapidAPI-Key': 'ebc3c9ea22msha6fdc66c3bcac79p10e332jsn88cb60266546',
+        'X-RapidAPI-Host': 'chicmi.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log("Fetched events:", response.data);
+
+      this.events = response.data; // Assign the response data to the events property
     } catch (error) {
-      console.error("Error fetching products:", error);
+      console.error("Error fetching events:", error);
     }
   }
 }
